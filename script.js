845cfb9d73df56f65e59e632fa0b2bbc8b03fc69
chandni <thechandni5@gmail.com>
2024-06-02 16:11:12 +0530

    page1 complete

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
-var tl = gsap.timeline();
+function loadingAnimation(){
 
-gsap.from("#loader h1",{
-    y: 100,
-    opacity: 0,
-    duration: 3.2,
-    delay: 0.01,
-    ease: "power4.inOut"
-})
+    var tl = gsap.timeline();
 
-tl.from("#counter h2",{
-    opacity: 0,
-    delay: 0.3,
-    onStart: function(){
-        var h2 = document.querySelector("#counter h2")
-        var count = 0;
-        setInterval(function(){
-        if(count<100){
-        count++;
-        h2.innerHTML = count
-    }else{
-        h2.innerHTML = count;
-    }
-    },30);
-    },
-})
+    gsap.from("#loader h1",{
+        y: 100,
+        opacity: 0,
+        duration: 3.2,
+        delay: 0.01,
+        ease: "power4.inOut"
+    })
+    
+    tl.from("#counter h2",{
+        opacity: 0,
+        delay: 0.3,
+        onStart: function(){
+            var h2 = document.querySelector("#counter h2")
+            var count = 0;
+            setInterval(function(){
+            if(count<100){
+            count++;
+            h2.innerHTML = count
+        }else{
+            h2.innerHTML = count;
+        }
+        },30);
+        },
+    })
+    
+    tl.to("#loader", {
+        duration: 2,
+        delay: 2.5,
+        onComplete: function() {
+          var loader = document.getElementById('loader')
+          loader.style.display = 'none';
+        }
+      })
+    
+    tl.from("#page1", {
+        y: 1000,
+        duration: 0.7
+      })
+    
+    tl.from("#nav",{
+        opacity: 0
+    })
 
-tl.to("#loader", {
-    duration: 2,
-    delay: 2.5,
-    onComplete: function() {
-      var loader = document.getElementById('loader')
-      loader.style.display = 'none';
-    }
-  })
+    tl.from("#hero",{
+        opacity: 0
+    })
+}
 
-tl.from("#page1", {
-    y: 1000,
-    duration: 0.7
-  })
+loadingAnimation()
 
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#cursor",{
+            top:dets.y +"px",
+            left:dets.x + "px"
+        })
+    })
+}
+
+cursorAnimation()
